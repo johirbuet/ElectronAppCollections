bbbb07d87e1e61a8fd8b4ea98b69d1101834a088
Johirul Islam <johirbuet@gmail.com>
2017-06-18 18:54:35 -0500

    two apps

---
+++ ReadyToShow/index.js
@@ -0,0 +1,31 @@
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+
+
+app.on('ready', function() {
+    var appWindow, infoWindow;
+    appWindow = new BrowserWindow({
+        show: false
+    });
+    appWindow.loadURL('http://raybo.org');
+
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        transparent: true,
+        show: false,
+        frame: false
+    });
+
+    infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+    appWindow.once('ready-to-show', function() {
+        appWindow.show();
+        setTimeout(function() {
+            infoWindow.show();
+            setTimeout(function() { infoWindow.hide();}, 2000);
+        }, 1000);
+    });
+});
+
